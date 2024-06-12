ad05771f736908935e39e733a2165927bf846f3f
_Kerman <kermanx@qq.com>
2024-06-12 23:44:53 +0800

    refactor: directory structure

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@
 
 import { createApp } from 'vue'
 // @ts-ignore
-import Main from '../README.md'
+import Main from './README.md'
 import 'katex/dist/katex.min.css'
 import 'uno.css'
 import './styles.css'
-import Figure from './Figure.vue'
+import Figure from './components/Figure.vue'
 
 createApp(Main).component('Figure', Figure).mount('#app')
